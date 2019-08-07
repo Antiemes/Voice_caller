5e8a001af81e9764bf401ad0a8e753f37019676f
Antiemes <vg@electrit.hu>
2019-08-07 13:41:55 +0200

    Removed mispelled directory

--- firmwave/make.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-avr-gcc -mmcu="atmega328p" -D__AVR__ spi.c flash.c main.c -Wall -o main.elf -O2
-avr-size -C --mcu=atmega328p main.elf
-avr-objcopy -j .text -j .data -O ihex main.elf main.hex
-
--- firmwave/prog.sh
+++
@@ -1,2 +0,0 @@
-sudo avrdude -c arduino -p m328p -P /dev/ttyUSB0 -Uflash:w:main.hex:i -B 3
-#sudo avrdude -c avrispmkII -p t45 -P usb -U lfuse:w:0xe2:m -U hfuse:w:0xdf:m -U efuse:w:0xff:m 
