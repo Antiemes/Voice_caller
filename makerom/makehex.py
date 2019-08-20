f5da4a5e52eb5f8727474c3e4a934f73659efc01
Gergely VAKULYA <vakulya@dcs.uni-pannon.hu>
2019-08-20 22:28:36 +0200

    Script makes text file for flash upload.

---
+++ makerom/makehex.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import sys
+
+addr=0x100000
+chunksize=128
+blocksize=65536
+blockfree=0
+with open(sys.argv[1], "rb") as f:
+    while True:
+      chunk = f.read(128)
+      if chunk:
+        if (blockfree==0):
+          print("E", end='')
+          print('{:08x}'.format(addr), end='')
+          print('')
+          blockfree=blocksize
+        print("W", end='')
+        print('{:08x}'.format(addr), end='')
+        print('{:08x}'.format(128), end='')
+        for b in chunk:
+          print('{:02x}'.format(b), end='')
+        print('')
+        addr+=chunksize
+        blockfree-=chunksize
+      else:
+        break
